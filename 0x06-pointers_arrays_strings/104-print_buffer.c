530f4fa8bd2315f9b3c679b7da89325c0731baad
Omniaibrahim93 <133152394+Omniaibrahim93@users.noreply.github.com>
2023-08-26 22:35:16 +0800

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_line - prints a s bytes of a buffer
- * @l: line of buffer to print
- * @c: buffer to print
- * @s: bytes of buffer to print
+ * print_line_helper - Prints a line of bytes from a buffer
+ * @c: Buffer to print from
+ * @s: Number of bytes to print
+ * @l: Line number
+ *
  * Return: void
  */
-
 void print_line_helper(char *c, int s, int l)
 {
 	int x, k;
+
 	for (x = 0; x <= 9; x++)
 	{
 		if (x <= s)
-		       	printf("%02x", c[l * 10 + x]);
+			printf("%02x", c[l * 10 + x]);
 		else
 			printf("  ");
 		if (x % 2)
@@ -31,25 +32,23 @@ void print_line_helper(char *c, int s, int l)
 }
 
 /**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
+ * print_buffer - Prints a buffer
+ * @b: Buffer to print
+ * @size: Size of the buffer
+ *
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
 	int x;
+
 	for (x = 0; x <= (size - 1) / 10 && size; x++)
 	{
 		printf("%08x: ", x * 10);
 		if (x < size / 10)
-		{
 			print_line_helper(b, 9, x);
-		}
 		else
-		{
 			print_line_helper(b, size % 10 - 1, x);
-		}
 		putchar('\n');
 	}
 	if (size == 0)
